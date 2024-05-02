d15436bc2f281ac93e3acb14615206bb50896454
KZM-Bhai <mohmd786rehman@gmail.com>
2024-05-02 12:11:03 +0500

    Tried Database Connection, Fatal Error

--- checklogin.php
+++ checklogin.php
@@ -1,4 +1,31 @@
 <?php
+// echo $email." ".$password;
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "invoicing";
+
 $email = $_POST['email'];
 $password = $_POST['password'];
-echo $email." ".$password;
+
+// Create connection
+$con = mysqli_connect($servername,$username,$password,$dbname);
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+
+$sql = "SELECT * FROM users WHERE email = $email AND password = $password";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " " . $row["email"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
